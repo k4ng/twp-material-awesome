358f9533bcfdb038fa35d0707399a3c9d4a97003
cahya dyazin <cahyadyazin@yahoo.com>
2016-04-14 09:27:43 +0700

    delete

--- searchform.php
+++
@@ -1,7 +0,0 @@
-<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
-	<label>
-		<span></span>
-		<input type="search" placeholder="Entry keyword..." class="search_field" value="<?php echo get_search_query() ?>" name="s" />
-	</label>
-	<input type="submit" class="search_submit" value="Search" />
-</form>
